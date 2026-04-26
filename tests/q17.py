84d6a6addee7cc41e75749882fddfd896d60dffb
aakikip <poudelaakriti@gmail.com>
2026-04-25 20:39:56 -0700

    Update .gitignore

--- tests/q17.py
+++
@@ -1,5 +0,0 @@
-OK_FORMAT = True
-
-test = {   'name': 'q17',
-    'points': None,
-    'suites': [{'cases': [{'code': '>>> assert np.isclose(roc_auc, 0.8244, atol=0.001)\n', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
